acc225bf34037fd7bc32c4588a43f372b1d41fbd
luis.campos <gucampos845@gmail.com>
2020-05-12 19:57:19 -0300

    Teste do avaliador

--- src/components/Carrinho.js
+++ src/components/Carrinho.js
@@ -3,6 +3,7 @@ import Image from '../image/carrinho.png';
 
 class Carrinho extends React.Component {
   render() {
+    
     return (
       <div>
         <img src={Image} Alt="Carrinho de Compras" data-testid="shopping-cart-button" />
