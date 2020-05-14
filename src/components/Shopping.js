7304356601e3b6073845fac5c9931cff24e6918c
Lucas Franca <luc.rangel90@gmail.com>
2020-05-14 13:07:02 -0300

    cc

--- src/components/Shopping.js
+++ src/components/Shopping.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Image from '../image/box.png';
-import ImageBack from '../image/back.png'
+import ImageBack from '../image/back.png';
 
 export class Shopping extends Component {
   render() {
