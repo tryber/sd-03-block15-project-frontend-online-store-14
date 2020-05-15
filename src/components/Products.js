a4a937e4e5e29f6c1e47a3535efa027b297cb3af
Lucas Franca <luc.rangel90@gmail.com>
2020-05-15 16:22:04 -0300

    componente product criado

---
+++ src/components/Products.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+
+export class ProductDetail extends Component {
+  render() {
+    const { location: { state: { price, thumbnail, title } } } = this.props;
+    return ();
+  }
+
+export default ProductDetail;
