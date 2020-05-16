18afcf99fd271df4f1d069d76ff1912ea4fa84f5
Lucas Franca <luc.rangel90@gmail.com>
2020-05-16 19:40:42 -0300

    ainda não esta funcionando

--- src/services/api.js
+++ src/services/api.js
@@ -6,6 +6,20 @@ export async function getCategories() {
   return data;
 }
 
+export async function getCategory(categoryId) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
+
+export async function getQuery(query) {
+  const URL = `https://api.mercadolibre.com/sites/MLB/search?q=${query}`;
+  const response = await fetch(URL);
+  const data = await response.json();
+  return data;
+}
+
 export async function getProductsFromCategoryAndQuery(categoryId, query) {
   // implement here
   const id = `https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`;
